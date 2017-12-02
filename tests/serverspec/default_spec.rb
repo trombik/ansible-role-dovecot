bba54d38c104a9ab92bcb4f5d66bbd9ccd52111f
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-12-02 14:56:15 +0700

    [feature] initial implementation (#1)
    * initial implementation
    
    * rubocopfy

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -3,46 +3,84 @@ require "serverspec"
 
 package = "dovecot"
 service = "dovecot"
-config  = "/etc/dovecot/dovecot.conf"
-user    = "dovecot"
-group   = "dovecot"
-ports   = [PORTS]
-log_dir = "/var/log/dovecot"
-db_dir  = "/var/lib/dovecot"
+config_dir = "/etc/dovecot"
+ports = [143]
+default_owner = "root"
+default_group = "root"
+base_dir = "/var/run/dovecot"
 
 case os[:family]
 when "freebsd"
-  config = "/usr/local/etc/dovecot.conf"
-  db_dir = "/var/db/dovecot"
+  config_dir = "/usr/local/etc/dovecot"
+  default_group = "wheel"
+when "openbsd"
+  default_group = "wheel"
 end
+
+config = "#{config_dir}/dovecot.conf"
+confd_dir = "#{config_dir}/conf.d"
 
 describe package(package) do
   it { should be_installed }
 end
 
+describe file(confd_dir) do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by default_owner }
+  it { should be_grouped_into default_group }
+  it { should be_mode 755 }
+end
+
+["auth.conf"].each do |f|
+  describe file("#{confd_dir}/#{f}") do
+    it { should exist }
+    it { should be_file }
+    it { should be_owned_by default_owner }
+    it { should be_grouped_into default_group }
+    it { should be_mode 640 }
+    its(:content) { should match(/^disable_plaintext_auth = no$/) }
+    passdb_driver = case os[:family]
+                    when "openbsd"
+                      "bsdauth"
+                    else
+                      "pam"
+                    end
+
+    its(:content) { should match(/^passdb {\n\s+driver = #{passdb_driver}\n}\nuserdb {\n\s+driver = passwd\n}$/) }
+  end
+end
+
 describe file(config) do
+  it { should exist }
   it { should be_file }
-  its(:content) { should match Regexp.escape("dovecot") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
+  it { should be_owned_by default_owner }
+  it { should be_grouped_into default_group }
+  it { should be_mode 644 }
+  its(:content) { should match(/^protocols = imap$/) }
+  its(:content) { should match(/^listen = \*$/) }
+  its(:content) { should match(/^base_dir = "#{Regexp.escape(base_dir)}"$/) }
+  ["auth.conf"].each do |conf|
+    its(:content) { should match(/^!include #{Regexp.escape(confd_dir + "/#{conf}")}$/) }
+  end
 end
 
 case os[:family]
 when "freebsd"
   describe file("/etc/rc.conf.d/dovecot") do
+    it { should exist }
     it { should be_file }
+    it { should be_owned_by default_owner }
+    it { should be_grouped_into default_group }
+    it { should be_mode 644 }
+    its(:content) { should match(/^dovecot_config="#{config}"$/) }
+    its(:content) { should match(/^dovecot_flags=""$/) }
+  end
+when "openbsd"
+  describe command("rcctl get #{service} flags") do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should eq "" }
+    its(:stderr) { should eq "" }
   end
 end
 
