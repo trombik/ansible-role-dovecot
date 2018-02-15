81a4be88422b97c0c2b90da0b631b4784c74080a
Tomoyuki Sakurai <y@trombik.org>
2018-02-15 09:58:48 +0900

    [feature] support TLS/SSL
    also:
    
    * drop openbsd-snapshot-amd64

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -4,7 +4,7 @@ require "serverspec"
 package = "dovecot"
 service = "dovecot"
 config_dir = "/etc/dovecot"
-ports = [143]
+ports = [993]
 user = "dovecot"
 default_owner = "root"
 default_group = "root"
@@ -22,6 +22,7 @@ end
 
 config = "#{config_dir}/dovecot.conf"
 confd_dir = "#{config_dir}/conf.d"
+ssl_cert_dir = "#{config_dir}/ssl"
 
 describe package(package) do
   it { should be_installed }
@@ -34,6 +35,23 @@ describe user(user) do
   end
 end
 
+["dovecot_pub.pem", "dovecot_key.pem"].each do |f|
+  describe file("#{ssl_cert_dir}/#{f}") do
+    it { should exist }
+    it { should be_file }
+    it { should be_owned_by user }
+    it { should be_grouped_into default_group }
+    case f
+    when "dovecot_key.pem"
+      it { should be_mode 400 }
+      its(:content) { should match(/^-----BEGIN RSA PRIVATE KEY-----$/) }
+    when "dovecot_pub.pem"
+      it { should be_mode 444 }
+      its(:content) { should match(/^-----BEGIN CERTIFICATE-----$/) }
+    end
+  end
+end
+
 describe file(confd_dir) do
   it { should exist }
   it { should be_directory }
@@ -42,22 +60,29 @@ describe file(confd_dir) do
   it { should be_mode 755 }
 end
 
-["auth.conf"].each do |f|
+["auth.conf", "ssl.conf"].each do |f|
   describe file("#{confd_dir}/#{f}") do
     it { should exist }
     it { should be_file }
     it { should be_owned_by default_owner }
     it { should be_grouped_into default_group }
     it { should be_mode 640 }
-    its(:content) { should match(/^disable_plaintext_auth = no$/) }
-    passdb_driver = case os[:family]
-                    when "openbsd"
-                      "bsdauth"
-                    else
-                      "pam"
-                    end
+    case f
+    when "auth.conf"
+      its(:content) { should match(/^disable_plaintext_auth = yes$/) }
+      passdb_driver = case os[:family]
+                      when "openbsd"
+                        "bsdauth"
+                      else
+                        "pam"
+                      end
 
-    its(:content) { should match(/^passdb {\n\s+driver = #{passdb_driver}\n}\nuserdb {\n\s+driver = passwd\n}$/) }
+      its(:content) { should match(/^passdb {\n\s+driver = #{passdb_driver}\n}\nuserdb {\n\s+driver = passwd\n}$/) }
+    when "ssl.conf"
+      its(:content) { should match(/^ssl = required$/) }
+      its(:content) { should match(/^ssl_cert = <#{Regexp.escape("#{config_dir}/ssl/dovecot_pub.pem")}$/) }
+      its(:content) { should match(/^ssl_key = <#{Regexp.escape("#{config_dir}/ssl/dovecot_key.pem")}$/) }
+    end
   end
 end
 
@@ -67,7 +92,7 @@ describe file(config) do
   it { should be_owned_by default_owner }
   it { should be_grouped_into default_group }
   it { should be_mode 644 }
-  its(:content) { should match(/^protocols = imap$/) }
+  its(:content) { should match(/^protocols = imaps$/) }
   its(:content) { should match(/^listen = \*$/) }
   its(:content) { should match(/^base_dir = "#{Regexp.escape(base_dir)}"$/) }
   ["auth.conf"].each do |conf|
@@ -108,4 +133,22 @@ ports.each do |p|
   describe port(p) do
     it { should be_listening }
   end
+end
+
+describe port(143) do
+  it { should_not be_listening }
+end
+
+stderr_text = "depth=0 C = TH, ST = Bangkok, O = Internet Widgits Pty Ltd, CN = a.mx.trombik.org
+verify error:num=18:self signed certificate
+verify return:1
+depth=0 C = TH, ST = Bangkok, O = Internet Widgits Pty Ltd, CN = a.mx.trombik.org
+verify return:1
+DONE\n"
+describe command("echo | openssl s_client -connect localhost:imaps") do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq stderr_text }
+  its(:stdout) { should match(/^#{Regexp.escape("* OK [CAPABILITY IMAP4rev1 LITERAL+ SASL-IR LOGIN-REFERRALS ID ENABLE IDLE AUTH=PLAIN] Dovecot ready.")}$/) }
+  its(:stdout) { should match(/^#{Regexp.escape("subject=/C=TH/ST=Bangkok/O=Internet Widgits Pty Ltd/CN=a.mx.trombik.org")}$/) }
+  its(:stdout) { should match(/^#{Regexp.escape("issuer=/C=TH/ST=Bangkok/O=Internet Widgits Pty Ltd/CN=a.mx.trombik.org")}$/) }
 end
